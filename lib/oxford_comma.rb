4c55dc02c389d0a39a61015d3292b1e146ab6969
Dwight Marie <dwightmarie@gmail.com>
2019-10-21 12:03:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+  if array.length == 1
+   array.join
+  
+  elsif array.length == 2
+   array.join(' and ')
+  
+  elsif array.length == 3
+ array.pop()
+money = array.join(', ')
+money << " and"
+money << " starfruit"
+money.insert(32, ',') 
 
+else array.length > 3
+  array.pop()
+mud = array.join(', ')
+mud << " and"
+mud << " dragon fruits"
+mud.insert(31, ',') 
+end
 end
